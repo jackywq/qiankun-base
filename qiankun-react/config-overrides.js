1a6dcf7eddc174a4fe66d76d030109f4c0fd2f38
王权 <17612172372@163.com>
2024-12-10 19:45:03 +0800

    chore: 基座应用启动脚本修改

--- qiankun-react/config-overrides.js
+++ qiankun-react/config-overrides.js
@@ -1,17 +1,19 @@
 module.exports = {
-    webpack: config => {
-        config.output.library = 'reactApp';
-        config.output.libraryTarget = 'umd';
-        config.output.publicPath = 'http://localhost:20000/';
-        return config;
-    },
-    devServer: configFunction => {
-        return function (proxy, allowedHost) {
-            const config = configFunction(proxy, allowedHost);
-            config.headers = {
-                'Access-Control-Allow-Origin': '*'
-            }
-            return config;
-        }
-    }
-}
+  webpack: (config) => {
+    config.output.library = "reactApp";
+    config.output.libraryTarget = "umd";
+    config.output.publicPath = "http://localhost:20000/";
+    // config.output.open = false;
+    return config;
+  },
+  devServer: (configFunction) => {
+    return function(proxy, allowedHost) {
+      const config = configFunction(proxy, allowedHost);
+      config.headers = {
+        "Access-Control-Allow-Origin": "*",
+      };
+      config.open = false;
+      return config;
+    };
+  },
+};
